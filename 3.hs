97b1e04163e4a40df8a93de4dcc40a14aab95229
Ole Berg <jornodo@gmail.com>
2024-12-04 07:03:58 +0100

    Progress on ppppparsers

--- 3.hs
+++ 3.hs
@@ -1,15 +1,19 @@
--- Day 3
--- you get some input in the form of a string. The string have instructions, but anything that fits /mul(<Int>,<Int>)/ should match. 
--- Exceptions to this is ignored, such that /mul(<Int>,<Int>]/ would be invalid
--- parse all correct substrings, do the multiplication and sum the results.
-
 import System.IO
-import Text.Parsec (Parsec, try, anyChar, digit, char, string, (<|>), ParseError, many, parse)
+import Text.Parsec (Parsec, try, anyChar, digit, char, string, (<|>), ParseError, many, parse, optionMaybe )
 import qualified Text.Parsec as Parsec
 import Text.Parsec.String (Parser)
 import Control.Applicative (optional)
 import Data.Maybe (catMaybes)
 import Utils (parseLine)
+
+-- Day 3
+-- you get some input in the form of a string. The string has instructions, but anything that fits /mul(<Int>,<Int>)/ should match. 
+-- Exceptions to this are ignored, such that /mul(<Int>,<Int>]/ would be invalid.
+-- parse all correct substrings, do the multiplication and sum the results.
+
+-- Part 2
+-- if we parse don't() then any subsequent values should be ignored until we parse do(), which makes us resume parsing mul(x,y)
+
 
 integer :: Parser Int
 integer = read <$> many digit
@@ -23,14 +27,38 @@ mulParser = do
     _ <- char ')'
     return (x, y)
 
+
+dontMulParser :: Parser (Int, Int)
+dontMulParser = do
+    _ <- string "mul("
+    x <- integer
+    _ <- char ','
+    y <- integer
+    _ <- char ')'
+    return (0, 0)
+
 safeMulParser :: Parser (Maybe (Int, Int))
 safeMulParser = (Just <$> try mulParser) <|> (anyChar *> pure Nothing)
 
 multipleMulParser :: Parser [(Int, Int)]
 multipleMulParser = catMaybes <$> many safeMulParser
 
-parser :: String -> Either ParseError [(Int, Int)]
-parser str = parse multipleMulParser "" str
+-- TODO: the last line in doParser and dontParser only checks first token for do() or dont() before repeatedly finding mul() or discarding anyChar. 
+-- We still want to parse do or Don't, but mulParser and the skip anyChar parser returns a pair of int, while doParser and dontParser returns lists of these
+
+-- figure out how to optionally parse do() or don't() before parsing mul() while making this still dictate the value of a given mul()
+
+-- made doParser and dontParser only return pairs, issue is that they still expect do() or don't() before every other token, which doesn't consider possible multiples of other tokens inbetween mode switching tokens
+
+doParser :: Parser (Int, Int)
+doParser = do
+    _ <- try (string "do()")
+    ((try dontParser) <|> (try mulParser) <|> (anyChar >> pure (0, 0))) >>= return
+
+dontParser :: Parser (Int, Int)
+dontParser = do 
+    _ <- try (string "don't()")
+    ((try doParser) <|> (try dontMulParser) <|> (anyChar >> pure (0, 0))) >>= return
 
 productAndSum :: Either ParseError [(Int, Int)] -> Int
 productAndSum (Right tuples)    = sum $ map (\(x, y) -> x * y) tuples
@@ -39,8 +67,7 @@ productAndSum (Left _)          = 0
 main :: IO ()
 main = do
     -- part 1
-    content <- readFile "3.txt"
+    content <- readFile "3t.txt"
     let linesOfFile = lines content
     
-    print $ sum $ map productAndSum (map (parse multipleMulParser "") linesOfFile)
-
+    print $ map (parse (many (doParser <|> dontParser)) "") (map (\line -> "don't()" ++ line ) linesOfFile)
