47fe614f4195fa61459b0974137548071bc5469d
Ole Berg <jornodo@gmail.com>
2024-12-06 07:10:54 +0100

    Some logic for day 4

--- 4.hs
+++ 4.hs
@@ -1,4 +1,6 @@
 import Data.List (transpose, isPrefixOf, tails)
+import qualified Data.Set as Set
+import Data.Set (Set)
 
 -- day 4
 -- part 1:
@@ -8,24 +10,34 @@ import Data.List (transpose, isPrefixOf, tails)
 -- Function to extract top-left to bottom-right diagonals
 diagonalsTLBR :: [[a]] -> [[a]]
 diagonalsTLBR [] = []
-diagonalsTLBR xss = [diag i xss | i <- [0..(n + m - 2)]]
+diagonalsTLBR xss = [diag i xss | i <- [0 .. (n + m - 2)]]
   where
     n = length xss
     m = length (head xss)
-    diag k xss = [xss !! (k - j) !! j | j <- [max 0 (k - n + 1) .. min k (m - 1)]]
+    diag k xss = [xss !! (k - j) !! j | j <- [max 0 (k - n + 1) .. min k (m - 1)], (k - j) < n, j < m]
+
+diagTRBL :: [[a]] -> [[a]]
+diagTRBL [] = []
+diagTRBL xss = [diagTRBLFrom i xss | i <- [0 .. (n + m - 2)]]
+    where
+    n = length xss
+    m = length (head xss)
+    diagTRBLFrom k xss = [xss !! (j) !! (k - j) | j <- [max 0 (k - m + 1) .. min k (n - 1)], (k - j) >= 0, j < n]
+    
+    
+    
+
 
 extractAllDirectionalLines :: [String] -> [String]
 extractAllDirectionalLines grid = do
     let transposedGrid = transpose grid
     let diagonalGrid = diagonalsTLBR grid
-    let transposedDiagonalGrid = diagonalsTLBR (transpose grid)
+    let transposedDiagonalGrid = diagTRBL grid
 
-    let allDirections = concat [grid, transposedGrid, diagonalGrid, transposedDiagonalGrid]
-    -- include reverse as well since that is part of word search
-    concat [allDirections, (map reverse allDirections)]
+    concat [grid, transposedGrid, diagonalGrid, transposedDiagonalGrid]
 
 isPrefixedXMAS :: String -> Bool
-isPrefixedXMAS s = isPrefixOf "XMAS" s
+isPrefixedXMAS s = (isPrefixOf "XMAS" s) || (isPrefixOf "SAMX" s)
 
 countSubstring :: String -> Int
 countSubstring str = length $ filter isPrefixedXMAS (tails str)
@@ -37,8 +49,17 @@ countAllSubstrings grid = do
 
 main :: IO ()
 main = do
-    -- Load file content and parse each line with the parser
     content <- readFile "4.txt"
     let contentLines = lines content
 
+   -- mapM_ print (extractAllDirectionalLines contentLines)
+
     print $ countAllSubstrings contentLines
+    print $ length contentLines -- 10x10
+
+    let lines = extractAllDirectionalLines contentLines
+    print $ length lines -- 58 -> 10 + 10 + 19 + 19 
+    -- diag should be 2n - 1
+
+    let setLines = Set.fromList lines
+    print $ length lines
